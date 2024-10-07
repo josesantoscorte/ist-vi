35faf7231fd7984eb28ef3c955b8892372acbf43
Alessandro Campagna <alecostacamp@gmail.com>
2024-10-07 20:27:22 +0100

    its a working

--- idioms/linechart.js
+++ idioms/linechart.js
@@ -1,63 +1,133 @@
-// idioms/linechart.js
 function createLineChart(data) {
-  const margin = { top: 20, right: 30, bottom: 30, left: 40 },
-      width = 800 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
+    // Set the dimensions and margins of the graph
+    const margin = { top: 20, right: 30, bottom: 50, left: 60 },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
-  const svg = d3.select("body").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`);
+    // Append the svg object to the body of the page
+    const svg = d3.select(".LineChart")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  const x = d3.scaleTime()
-      .domain(d3.extent(data, d => new Date(d.year, 0, 1)))
-      .range([0, width]);
+    // Group the data by state and year
+    const nestedData = d3.group(data, d => d.state, d => d.year);
 
-  const y = d3.scaleLinear()
-      .domain([0, d3.max(data, d => d.incidents)])
-      .range([height, 0]);
+    // Create a list of states
+    const states = Array.from(nestedData.keys());
 
-  const line = d3.line()
-      .x(d => x(new Date(d.year, 0, 1)))
-      .y(d => y(d.incidents));
+    // Create a list of years
+    const years = Array.from(new Set(data.map(d => d.year))).sort();
 
-  const states = d3.groups(data, d => d.state);
+    // Create scales
+    const x = d3.scalePoint()
+        .domain(years)
+        .range([0, width]);
 
-  svg.append("g")
-      .attr("transform", `translate(0,${height})`)
-      .call(d3.axisBottom(x).ticks(d3.timeYear.every(1)).tickFormat(d3.timeFormat("%Y")));
+    let y = d3.scaleLinear()
+        .domain([0, d3.max(states, state => d3.max(years, year => nestedData.get(state).get(year)?.length || 0))])
+        .range([height, 0]);
 
-  svg.append("g")
-      .call(d3.axisLeft(y));
+    // Create axes
+    const xAxis = svg.append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x).tickFormat(d3.format("d")));
 
-  const tooltip = d3.select("#tooltip");
+    let yAxis = svg.append("g")
+        .call(d3.axisLeft(y));
 
-  svg.selectAll(".line")
-      .data(states)
-      .enter().append("path")
-      .attr("class", "line")
-      .attr("fill", "none")
-      .attr("stroke", "darkgray")
-      .attr("stroke-width", 1.5)
-      .attr("d", d => line(d[1]))
-      .on("mouseover", function(event, d) {
-          d3.select(this).attr("stroke", "yellow").attr("stroke-width", 3);
-          const totalIncidents = d3.sum(d[1], d => d.incidents);
-          tooltip.html(`
-              <strong>State:</strong> ${d[0]}<br>
-              <strong>Total Incidents:</strong> ${totalIncidents}
-          `)
-          .style("left", (event.pageX + 10) + "px")
-          .style("top", (event.pageY - 28) + "px")
-          .style("opacity", 1);
-      })
-      .on("mouseout", function() {
-          d3.select(this).attr("stroke", "darkgray").attr("stroke-width", 1.5);
-          tooltip.style("opacity", 0);
-      })
-      .on("mousemove", function(event) {
-          tooltip.style("left", (event.pageX + 10) + "px")
-                 .style("top", (event.pageY - 28) + "px");
-      });
+    // Create a color scale
+    const color = d3.scaleOrdinal(d3.schemeCategory10)
+        .domain(states);
+
+    // Create a tooltip
+    const tooltip = d3.select(".LineChart")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+        .style("position", "absolute");
+
+    // Draw the lines
+    function drawLines(filteredData, xScale, xDomain) {
+        svg.selectAll(".line").remove();
+
+        states.forEach(state => {
+            const stateData = xDomain.map(xValue => ({
+                xValue: xValue,
+                count: filteredData.get(state).get(xValue)?.length || 0,
+                injured: filteredData.get(state).get(xValue)?.reduce((sum, d) => sum + d.injured, 0) || 0,
+                killed: filteredData.get(state).get(xValue)?.reduce((sum, d) => sum + d.killed, 0) || 0
+            }));
+
+            const line = svg.append("path")
+                .datum(stateData)
+                .attr("class", "line")
+                .attr("fill", "none")
+                .attr("stroke", "darkgrey")
+                .attr("stroke-width", 1.5)
+                .attr("d", d3.line()
+                    .x(d => xScale(d.xValue))
+                    .y(d => y(d.count))
+                );
+
+            // Add hover functionality
+            line.on("mouseover", function(event, d) {
+                d3.select(this).attr("stroke", "yellow").attr("stroke-width", 3);
+                tooltip.style("opacity", 1);
+            })
+            .on("mousemove", function(event, d) {
+                const [xPos, yPos] = d3.pointer(event);
+                tooltip.html(`State: ${state}<br>Total Incidents: ${d3.sum(stateData, d => d.count)}`)
+                    .style("left", (xPos + 70) + "px")
+                    .style("top", (yPos) + "px");
+            })
+            .on("mouseout", function() {
+                d3.select(this).attr("stroke", "darkgrey").attr("stroke-width", 1.5);
+                tooltip.style("opacity", 0);
+            });
+        });
+    }
+
+    drawLines(nestedData, x, years);
+
+    // Add labels
+    svg.append("text")
+        .attr("text-anchor", "end")
+        .attr("x", width)
+        .attr("y", height + margin.top + 20)
+        .text("Year");
+
+    svg.append("text")
+        .attr("text-anchor", "end")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left + 20)
+        .attr("x", -margin.top)
+        .text("Number of Incidents");
+
+    // Add click event to x-axis ticks
+    xAxis.selectAll("text")
+        .style("cursor", "pointer")
+        .on("click", function(event, year) {
+            const filteredData = d3.group(data.filter(d => d.year === year), d => d.state, d => d.month);
+            const months = Array.from(new Set(data.filter(d => d.year === year).map(d => d.month))).sort();
+
+            const xMonth = d3.scalePoint()
+                .domain(months)
+                .range([0, width]);
+
+            xAxis.call(d3.axisBottom(xMonth).tickFormat(d3.timeFormat("%B")));
+
+            // Update y-axis scale
+            y.domain([0, d3.max(states, state => d3.max(months, month => filteredData.get(state).get(month)?.length || 0))]);
+            yAxis.transition().duration(1000).call(d3.axisLeft(y));
+
+            drawLines(filteredData, xMonth, months);
+        });
 }
--- script.js
+++ script.js
@@ -2,19 +2,6 @@ function init() {
   d3.csv("../data/parsed_gun.csv").then(data => {
     // Filter out the data where the year is 2013
     const filteredData = data.filter(d => +d.year !== 2013);
-  
-    // Aggregate data by year and state
-    const incidentsByYearAndState = d3.rollup(filteredData, v => v.length, d => +d.year, d => d.state);
-  
-    // Convert the nested map to an array of objects
-    const incidentsArray = [];
-    incidentsByYearAndState.forEach((states, year) => {
-        states.forEach((incidents, state) => {
-            incidentsArray.push({ year, state, incidents });
-        });
-    });
-  
-    // Create the line chart
-    createLineChart(incidentsArray);
+    createLineChart(filteredData);
   });
 }
