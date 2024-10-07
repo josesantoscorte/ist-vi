efcdb8522d51943aa90d8cbd8b576338c504e7a4
José Corte <josesantoscorte@gmail.com>
2024-10-07 18:29:40 +0100

    fix: remove 2013 and optimize code

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 function init() {
   d3.csv("../data/parsed_gun.csv").then(data => {
-    // Parse the data
-    const parseDate = d3.timeParse("%Y-%m-%d");
-    data.forEach(d => {
-        d.date = parseDate(d.date);
-        d.year = d.date.getFullYear();
-    });
-
     // Filter out the data where the year is 2013
-    const filteredData = data.filter(d => d.year !== 2013);
+    const filteredData = data.filter(d => +d.year !== 2013);
   
     // Aggregate data by year and state
-    const incidentsByYearAndState = d3.rollup(filteredData, v => v.length, d => d.year, d => d.state);
+    const incidentsByYearAndState = d3.rollup(filteredData, v => v.length, d => +d.year, d => d.state);
   
     // Convert the nested map to an array of objects
     const incidentsArray = [];
